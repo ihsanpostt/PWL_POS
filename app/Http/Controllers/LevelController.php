322c3b98d67c7df80f0c2f4cb611d3fdee0d9aa5
Burhanuddin ihsan <echan5886@gmail.com>
2026-03-05 10:17:37 +0700

    joobsheet 3 - praktikum 4

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class LevelController extends Controller
 {
-    //
+    public function index()
+    {
+        // DB::insert('INSERT INTO m_level (level_kode, level_nama, created_at) VALUES (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        // return 'Insert Data Baru Berhasil!';
+
+        // $row = DB::update('UPDATE m_level SET level_nama = ? WHERE level_kode = ?', ['Customer', 'CUS']);
+        // return 'Update Data Berhasil! Jumlah data yang diupdate: ' . $row . ' baris.';
+        
+        // $row = DB::delete('DELETE FROM m_level WHERE level_kode = ?', ['CUS']);
+        // return 'Delete Data Berhasil! Jumlah data yang dihapus: ' . $row . ' baris.';
+
+        $data = DB::select('SELECT * FROM m_level');
+        return view('level', ['data' => $data]);
+        }
 }
