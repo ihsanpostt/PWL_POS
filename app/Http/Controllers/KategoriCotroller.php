84feb427f061a6e9df6df5d6101ecb06e3d7c5a7
Burhanuddin ihsan <echan5886@gmail.com>
2026-03-05 10:17:02 +0700

    jobsheet 3 - praktikum 5

--- app/Http/Controllers/KategoriCotroller.php
+++ app/Http/Controllers/KategoriCotroller.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-class KategoriCotroller extends Controller
+class KategoriController extends Controller
 {
-    //
+    public function index()
+    {
+        // $data = [
+        //     'kategori_kode' => 'JJN',
+        //     'kategori_nama' => 'Snack/Makanan Ringan',
+        //     'created_at' => now()
+        // ];
+        // DB::table('m_kategori')->insert($data);
+        // return 'Insert Data Baru Berhasil!';
+
+        // $row = DB::table('m_kategori')
+        //     ->where('kategori_kode', 'JJN')
+        //     ->update(['kategori_nama' => 'Camilan']);
+        // return "Update data berhasil, jumlah data yang diupdate: " . $row . " baris.";
+    
+        // $row = DB::table('m_kategori')
+        //     ->where('kategori_kode', 'JJN')
+        //     ->delete();
+        // return "Delete data berhasil, jumlah data yang dihapus: " . $row . " baris.";
+
+        $data = DB::table('m_kategori')->get();
+        return view('kategori', ['data' => $data]);
+    }
 }
